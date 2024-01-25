c8efa00459c9d39657cc041eea6333784cfaa4cb
rulen111 <akhmarovri@yandex.ru>
2024-01-25 21:59:23 +0300

    Added keyboard and handler class

--- func/bot.py
+++ func/bot.py
@@ -1,6 +1,21 @@
 import vk_api
 from random import randrange
 from datetime import date
+import yaml
+import datetime
+
+with open("config.yaml") as c:
+    config = yaml.full_load(c)
+
+VK_USER_TOKEN = config["VK"]["USER_TOKEN"]
+VK_VERSION = config["VK"]["VERSION"]
+VK_APP_ID = config["VK"]["APP_ID"]
+VK_GROUP_TOKEN = config["VK"]["GROUP_TOKEN"]
+
+KB_MAIN = "func/keyboards/keyboard_main.json"
+KB_CHOOSE = "func/keyboards/keyboard_choose.json"
+KB_SEARCH = "func/keyboards/keyboard_search.json"
+KB_EMPTY = "func/keyboards/keyboard_empty.json"
 
 
 def calculate_age(born):
@@ -20,12 +35,19 @@ def get_auth_link(app_id, scope, version):
 
 
 class VKClient(vk_api.VkApi):
-    def write_msg(self, user_id, message, attachment=None):
-        if attachment:
-            self.method('messages.send', {'user_id': user_id, 'message': message,
-                                          'attachment': attachment, 'random_id': randrange(10 ** 7), })
+    def set_token(self, token):
+        self.token = token
+
+    def write_msg(self, user_id, message, fields=None):
+        values = {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7)}
+        if fields:
+            self.method('messages.send', {**values, **fields})
         else:
-            self.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7), })
+            self.method('messages.send', values)
+
+    def event_answer(self, event_id, user_id, peer_id):
+        self.method("messages.sendMessageEventAnswer", {"event_id": event_id, "user_id": user_id,
+                                                        "peer_id": peer_id})
 
     def get_user_info(self, user_ids, fields):
         user_info = self.method("users.get", {"user_ids": user_ids, "fields": fields})
@@ -36,10 +58,7 @@ class VKClient(vk_api.VkApi):
             "sort": 0,
             "offset": 0,
             "count": 10,
-            "city": fields.get("city_id", 1),
-            "sex": fields.get("sex", 1),
-            "age_from": fields.get("age_from", 18),
-            "age_to": fields.get("age_to", 99),
+            **fields,
             "has_photo": 1
         }
         search_res = self.method("users.search", params)
@@ -61,3 +80,75 @@ class VKClient(vk_api.VkApi):
         result = sorted(photos, key=lambda x: x.get("likes", 0), reverse=True)
         return result[:count]
 
+
+class VKhandler():
+    def __init__(self, group_client: VKClient):
+        self.client = group_client
+        self.user_client = None
+        self.search_fields = {}
+
+    def add_user_client(self, user_client: VKClient):
+        self.user_client = user_client
+
+    def set_search_fields(self, fields):
+        for k, v in fields.items():
+            self.search_fields[k] = v
+
+    def handle_start(self, event):
+        self.client.write_msg(event.object.user_id, f"Для начала работы с ботом необходимо отправить ему "
+                                                            f"токен авторизации. Получить токен можно по ссылке "
+                                                            f"ниже, выдав необходимые права боту. Значение токена "
+                                                            f"будет записано в адресе страницы успешной авторизации "
+                                                            f"в поле 'access_token'",
+                              {"attachment": get_auth_link(VK_APP_ID, "photos", VK_VERSION)})
+
+    def handle_token(self, event):
+        self.add_user_client(VKClient(token=event.obj.message['text'], app_id=VK_APP_ID))
+        # self.user_client.auth(reauth=False, token_only=True)
+        self.client.write_msg(event.object.user_id, f"Вы успешно авторизованы",
+                              {"keyboard": open(KB_MAIN, 'r', encoding='UTF-8').read()})
+
+    def handle_authorize(self, event):
+        self.client.event_answer(event.object.event_id, event.object.user_id, event.object.peer_id)
+        self.client.write_msg(event.obj.peer_id, f"Для начала работы с ботом необходимо отправить ему "
+                                                            f"токен авторизации. Получить токен можно по ссылке "
+                                                            f"ниже, выдав необходимые права боту. Значение токена "
+                                                            f"будет записано в адресе страницы успешной авторизации "
+                                                            f"в поле 'access_token'",
+                              {"attachment": get_auth_link(VK_APP_ID, "photos", VK_VERSION)})
+
+    def handle_search(self, event):
+        user_info = self.client.get_user_info(event.object.user_id, "bdate,sex,city")[0]
+        city = user_info.get("city", {})
+        sex = user_info.get("sex", 0)
+        born = datetime.datetime.strptime(user_info.get("bdate", ""), "%d.%m.%Y")
+        age = calculate_age(born)
+        search_fields = {
+            "city": city.get("id", 2),
+            "sex": 0 if sex == 1 else 1,
+            "age_from": age,
+            "age_to": age,
+        }
+        self.set_search_fields(search_fields)
+        self.client.event_answer(event.object.event_id, event.object.user_id, event.object.peer_id)
+        self.client.write_msg(event.object.user_id, f"Выполнить поиск по следующим данным?:\n- г. "
+                                                            f"{city.get('title', '')}\n- {age} лет\n- пол "
+                                                            f"{'мужской' if sex == 1 else 'женский'}",
+                              {"keyboard": open(KB_SEARCH, 'r', encoding='UTF-8').read()})
+
+    def handle_main_menu(self, event):
+        self.client.event_answer(event.object.event_id, event.object.user_id, event.object.peer_id)
+        self.client.write_msg(event.object.user_id, f"Главное меню", {"keyboard": open(KB_MAIN, 'r', encoding='UTF-8').read()})
+
+    def handle_start_search(self, event):
+
+        fields = self.search_fields
+        search_res = self.user_client.search_users(fields)
+        photos = self.user_client.get_top_photos(search_res[1].get("id", 1))
+
+        self.client.event_answer(event.object.event_id, event.object.user_id, event.object.peer_id)
+        self.client.write_msg(event.object.user_id, f"{search_res[1].get('first_name', '')}"
+                                                            f" {search_res[1].get('last_name', '')}",
+                              {"attachment": f"{photos[0].get('url', '')},"
+                                             f"{photos[1].get('url', '')},"
+                                             f"{photos[2].get('url', '')}"})
--- main.py
+++ main.py
@@ -1,59 +1,36 @@
-from func.bot import *
+from func.bot import VKClient, VKhandler
 import yaml
-from vk_api.longpoll import VkLongPoll, VkEventType
-import datetime
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 
 with open("config.yaml") as c:
     config = yaml.full_load(c)
 
-VK_USER_TOKEN = config["VK"]["USER_TOKEN"]
 VK_VERSION = config["VK"]["VERSION"]
-VK_APP_ID = config["VK"]["APP_ID"]
 VK_GROUP_TOKEN = config["VK"]["GROUP_TOKEN"]
+VK_GROUP_ID = config["VK"]["GROUP_ID"]
 
 
 if __name__ == "__main__":
-    # user_id = "73489272"
-    vk_group_client = VKClient(token=VK_GROUP_TOKEN)
-    longpoll = VkLongPoll(vk_group_client)
+    vk_group_client = VKClient(token=VK_GROUP_TOKEN, api_version=VK_VERSION)
+    vk_handler = VKhandler(vk_group_client)
+    longpoll = VkBotLongPoll(vk_group_client, VK_GROUP_ID)
 
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
+        if event.type == VkBotEventType.MESSAGE_EVENT:
+            if event.object.payload.get("type", "") == "authorize":
+                vk_handler.handle_authorize(event)
+            elif event.object.payload.get("type", "") == "search":
+                vk_handler.handle_search(event)
+            elif event.object.payload.get("type", "") == "search_start":
+                vk_handler.handle_start_search(event)
+            elif event.object.payload.get("type", "") == "main_menu":
+                vk_handler.handle_main_menu(event)
 
-            if event.to_me:
-                request = event.text
+        elif event.type == VkBotEventType.MESSAGE_NEW:
+            if event.from_user:
+                if event.obj.message['text'].startswith("vk1.a."):
+                    vk_handler.handle_token(event)
+                elif event.obj.message['text'] == "Начать":
+                    vk_handler.handle_start(event)
 
-                if request == "Начать":
-                    vk_group_client.write_msg(event.user_id, f"Хай, {event.user_id}")
-                    user_info = vk_group_client.get_user_info(event.user_id, "bdate,sex,city")[0]
-                    city = user_info.get("city", {})
-                    sex = user_info.get("sex", 0)
-                    born = datetime.datetime.strptime(user_info.get("bdate", ""), "%d.%m.%Y")
-                    age = calculate_age(born)
-                    vk_group_client.write_msg(event.user_id, f"Выполнить поиск по следующим данным?:\n- г. "
-                                                             f"{city.get('title', '')}\n- {age} лет\n- пол "
-                                                             f"{'мужской' if sex == 1 else 'женский'}")
-                    for event in longpoll.listen():
-                        if event.type == VkEventType.MESSAGE_NEW:
-                            if event.to_me:
-                                request = event.text
-
-                                if request == "Да":
-                                    vk_user_client = VKClient(token=VK_USER_TOKEN, app_id=VK_APP_ID)
-                                    fields = {
-                                        "city_id": city.get("id", 2),
-                                        "sex": sex,
-                                        "age_from": age,
-                                        "age_to": age
-                                    }
-                                    search_res = vk_user_client.search_users(fields)
-                                    photos = vk_user_client.get_top_photos(search_res[1].get("id", 1))
-
-                                    vk_group_client.write_msg(event.user_id, f"{search_res[1].get('first_name', '')}"
-                                                                             f" {search_res[1].get('last_name', '')}",
-                                                              attachment=f"{photos[0].get('url', '')},"
-                                                                         f"{photos[1].get('url', '')},"
-                                                                         f"{photos[2].get('url', '')}")
-                else:
-                    vk_group_client.write_msg(event.user_id, "Для начала работы отправьте боту 'Начать'")
