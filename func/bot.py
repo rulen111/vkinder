b6ddab49cde00ffad675f6f4be882d3ea45a582e
rulen111 <akhmarovri@yandex.ru>
2024-01-28 14:48:07 +0300

    Initial rotation setup

--- func/bot.py
+++ func/bot.py
@@ -57,12 +57,17 @@ class VKClient(vk_api.VkApi):
         params = {
             "sort": 0,
             "offset": 0,
-            "count": 10,
+            "count": 100,
             **fields,
             "has_photo": 1
         }
         search_res = self.method("users.search", params)
-        return search_res.get("items", [])
+        items = search_res.get("items", [])
+        result = {item.get("id", 0): {"first_name": item.get("first_name", ""),
+                                      "last_name": item.get("last_name", ""),
+                                      "link": f"https://vk.com/id{item.get('id', 0)}"} for item in items}
+
+        return result
 
     def get_city_id(self, city_title):
         city = self.method("database.getCities", {"q": city_title, "need_all": 0})
@@ -72,49 +77,46 @@ class VKClient(vk_api.VkApi):
         album = self.method("photos.get", {"owner_id": owner_id, "album_id": "profile", "extended": 1})
         photos = []
         for item in album.get("items", []):
+            id = item.get("id", 0)
             url = item.get("sizes", [])[-1].get("url", "")
             likes = item.get("likes", {}).get("count", 0)
-            entry = {"url": url, "likes": likes}
+            entry = {"id": id, "url": url, "likes": likes}
             photos.append(entry)
 
         result = sorted(photos, key=lambda x: x.get("likes", 0), reverse=True)
         return result[:count]
 
 
-class VKhandler():
-    def __init__(self, group_client: VKClient):
+class VKhandler:
+    def __init__(self, group_client: VKClient, user_client: VKClient = None,
+                 search_fields: dict = {}, search_results: dict = {}, position=0):
         self.client = group_client
-        self.user_client = None
-        self.search_fields = {}
-
-    def add_user_client(self, user_client: VKClient):
         self.user_client = user_client
-
-    def set_search_fields(self, fields):
-        for k, v in fields.items():
-            self.search_fields[k] = v
+        self.search_fields = search_fields
+        self.search_results = search_results
+        self.position = position
 
     def handle_start(self, event):
         self.client.write_msg(event.object.user_id, f"Для начала работы с ботом необходимо отправить ему "
-                                                            f"токен авторизации. Получить токен можно по ссылке "
-                                                            f"ниже, выдав необходимые права боту. Значение токена "
-                                                            f"будет записано в адресе страницы успешной авторизации "
-                                                            f"в поле 'access_token'",
+                                                    f"токен авторизации. Получить токен можно по ссылке "
+                                                    f"ниже, выдав необходимые права боту. Значение токена "
+                                                    f"будет записано в адресе страницы успешной авторизации "
+                                                    f"в поле 'access_token'",
                               {"attachment": get_auth_link(VK_APP_ID, "photos", VK_VERSION)})
 
     def handle_token(self, event):
-        self.add_user_client(VKClient(token=event.obj.message['text'], app_id=VK_APP_ID))
+        self.user_client = VKClient(token=event.obj.message['text'], app_id=VK_APP_ID)
         # self.user_client.auth(reauth=False, token_only=True)
-        self.client.write_msg(event.object.user_id, f"Вы успешно авторизованы",
+        self.client.write_msg(event.obj.message['from_id'], f"Вы успешно авторизованы",
                               {"keyboard": open(KB_MAIN, 'r', encoding='UTF-8').read()})
 
     def handle_authorize(self, event):
         self.client.event_answer(event.object.event_id, event.object.user_id, event.object.peer_id)
         self.client.write_msg(event.obj.peer_id, f"Для начала работы с ботом необходимо отправить ему "
-                                                            f"токен авторизации. Получить токен можно по ссылке "
-                                                            f"ниже, выдав необходимые права боту. Значение токена "
-                                                            f"будет записано в адресе страницы успешной авторизации "
-                                                            f"в поле 'access_token'",
+                                                 f"токен авторизации. Получить токен можно по ссылке "
+                                                 f"ниже, выдав необходимые права боту. Значение токена "
+                                                 f"будет записано в адресе страницы успешной авторизации "
+                                                 f"в поле 'access_token'",
                               {"attachment": get_auth_link(VK_APP_ID, "photos", VK_VERSION)})
 
     def handle_search(self, event):
@@ -124,31 +126,44 @@ class VKhandler():
         born = datetime.datetime.strptime(user_info.get("bdate", ""), "%d.%m.%Y")
         age = calculate_age(born)
         search_fields = {
+            # "fields": "bdate,sex,city",
             "city": city.get("id", 2),
             "sex": 0 if sex == 1 else 1,
             "age_from": age,
             "age_to": age,
         }
-        self.set_search_fields(search_fields)
+        self.search_fields = search_fields
         self.client.event_answer(event.object.event_id, event.object.user_id, event.object.peer_id)
         self.client.write_msg(event.object.user_id, f"Выполнить поиск по следующим данным?:\n- г. "
-                                                            f"{city.get('title', '')}\n- {age} лет\n- пол "
-                                                            f"{'мужской' if sex == 1 else 'женский'}",
+                                                    f"{city.get('title', '')}\n- {age} лет\n- пол "
+                                                    f"{'мужской' if sex == 1 else 'женский'}",
                               {"keyboard": open(KB_SEARCH, 'r', encoding='UTF-8').read()})
 
     def handle_main_menu(self, event):
         self.client.event_answer(event.object.event_id, event.object.user_id, event.object.peer_id)
-        self.client.write_msg(event.object.user_id, f"Главное меню", {"keyboard": open(KB_MAIN, 'r', encoding='UTF-8').read()})
+        self.client.write_msg(event.object.user_id, f"Главное меню",
+                              {"keyboard": open(KB_MAIN, 'r', encoding='UTF-8').read()})
 
     def handle_start_search(self, event):
+        search_results = self.user_client.search_users(self.search_fields)
+        self.search_results = search_results
+        self.handle_rotation(event)
 
-        fields = self.search_fields
-        search_res = self.user_client.search_users(fields)
-        photos = self.user_client.get_top_photos(search_res[1].get("id", 1))
+    def handle_rotation(self, event):
+        search_entry = self.search_results[self.position]
 
+        photos = self.user_client.get_top_photos(search_entry.get("id", 1))
         self.client.event_answer(event.object.event_id, event.object.user_id, event.object.peer_id)
-        self.client.write_msg(event.object.user_id, f"{search_res[1].get('first_name', '')}"
-                                                            f" {search_res[1].get('last_name', '')}",
-                              {"attachment": f"{photos[0].get('url', '')},"
-                                             f"{photos[1].get('url', '')},"
-                                             f"{photos[2].get('url', '')}"})
+        self.client.write_msg(event.object.user_id, f"{search_entry.get('first_name', '')} "
+                                                    f"{search_entry.get('last_name', '')}",
+                              {"attachment": f"photo{photos[0].get('owner_id', 0)}_{photos[0].get('media_id', 0)},"
+                                             f"photo{photos[1].get('owner_id', 0)}_{photos[1].get('media_id', 0)},"
+                                             f"photo{photos[1].get('owner_id', 0)}_{photos[1].get('media_id', 0)}"})
+
+    # def handle_next(self, event):
+    #     self.position += 1
+    #     self.handle_rotation(event)
+    #
+    # def handle_like(self, event):
+    #
+
